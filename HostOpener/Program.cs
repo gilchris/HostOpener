b300734da80a6bfe6d3ab383d92fcfae2ca334c9
Changgun Lee <spamgilchris@gmail.com>
2016-12-07 23:08:51 +0900

    These are all

---
+++ HostOpener/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostOpener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = @"c:\Windows\System32\notepad.exe";
+            startInfo.Arguments = @"c:\Windows\System32\drivers\etc\hosts";
+            Process.Start(startInfo);
+        }
+    }
+}
